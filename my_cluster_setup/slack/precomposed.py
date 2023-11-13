a151eacc596e2b2995a0c45b876d68a7bfd2740b
Seung Hyun Kim <skim449@illinois.edu>
2023-11-13 09:44:15 -0600

    add slack message tool

---
+++ my_cluster_setup/slack/precomposed.py
@@ -0,0 +1,14 @@
+import os
+import requests
+
+def job_done():
+    # Get token from environment
+    token = os.environ['SLACK_BOT_TOKEN']
+
+    channel = 'nodes'
+    text = 'Job Done.\n    From: {os.uname()}'
+    bot_name = 'monitor'
+
+    payload = data = {'token': token, 'channel': channel, 'text': text, 'username': bot_name}
+    r = requests.post('https://slack.com/api/chat.postMessage', payload)
+    #print(r.text)
