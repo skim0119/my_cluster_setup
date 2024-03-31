20b301e57e294217b078e3ca201f72bbf3b33514
Seung Hyun Kim <skim0119@gmail.com>
2024-03-30 22:29:10 -0500

    feat: logging experiment

---
+++ my_cluster_setup/experiment_logging.py
@@ -0,0 +1,16 @@
+import os
+
+def collect_logfiles(path, tag="log"):
+    str_list = []
+    for root, dirs, files in os.walk(path):
+        for filename in files:
+            fpath = os.path.join(root, filename)
+            if tag not in filename:
+                continue
+            str_list.append(f"log path: {fpath}")
+            with open(fpath, "r") as log:
+                str_list.extend(log.readlines())
+            str_list.append("")
+            str_list.append("-"*80)
+            str_list.append("")
+    return '\n'.join(str_list)
